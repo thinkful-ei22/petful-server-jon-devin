a549a375686b8f980710b578ec5165034030dca5
dghero <devingreyhero@gmail.com>
2018-09-07 13:16:29 -0700

    Add reset endpoint to reseed database for demos

--- routes/reset.js
+++ routes/reset.js
@@ -2,20 +2,38 @@ const express = require('express');
 const Queue = require('../utils/queue');
 
 const seedCats = require('../db/seed/cats');
+const seedDogs = require('../db/seed/dogs');
 
 const Cat = require('../models/cat');
-// const Dog = require('../models/dog');
+const Dog = require('../models/dog');
 
 const router = express.Router();
 
 
 router.use('/', (req, res, next)=>{
 
-  Cat.findOne()
+  const catsArr = seedCats[0].cats;
+  const dogsArr = seedDogs[0].dogs;
+  console.log(catsArr);
+  console.log(dogsArr);
+
+  Cat.findOneAndUpdate({}, {cats: catsArr}, {})
     .then(results=>{
-      console.log(results);
       return res.json();
+    })
+    .catch(err =>{
+      next(err);
     });
+
+  Dog.findOneAndUpdate({}, {dogs: dogsArr}, {})
+    .then(results=>{
+      return res.json();
+    })
+    .catch(err =>{
+      next(err);
+    });
+
+  
 
   // Cat.findOneAndUpdate( {}, updateObj, options)
   //   .then(results =>{
